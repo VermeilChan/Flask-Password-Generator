26bc0807fea415660c3d8da4e0e3c47a8cab880a
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2024-05-04 04:45:20 +0100

    seperated flask code from password generator

--- src/app.py
+++ src/app.py
@@ -1,21 +1,7 @@
-from secrets import choice
-from string import ascii_lowercase, ascii_uppercase, digits, punctuation
 from flask import Flask, render_template, request
+from password_genrator import generate_password
 
 app = Flask(__name__)
-
-def generate_password(length=16, include_uppercase=True, include_lowercase=True, include_digits=True, include_special=True):
-    characters = ''
-    if include_uppercase:
-        characters += ascii_uppercase
-    if include_lowercase:
-        characters += ascii_lowercase
-    if include_digits:
-        characters += digits
-    if include_special:
-        characters += punctuation
-    generated_password = ''.join(choice(characters) for _ in range(length))
-    return generated_password
 
 @app.route('/')
 def index():
@@ -29,6 +15,7 @@ def generate():
     include_digits = 'digits' in request.form
     include_special = 'special' in request.form
     password = generate_password(password_length, include_uppercase, include_lowercase, include_digits, include_special)
+
     return password
 
 if __name__ == '__main__':
---
+++ src/password_genrator.py
@@ -0,0 +1,16 @@
+from secrets import choice
+from string import ascii_lowercase, ascii_uppercase, digits, punctuation
+
+def generate_password(length=16, include_uppercase=True, include_lowercase=True, include_digits=True, include_special=True):
+    characters = ''
+    if include_uppercase:
+        characters += ascii_uppercase
+    if include_lowercase:
+        characters += ascii_lowercase
+    if include_digits:
+        characters += digits
+    if include_special:
+        characters += punctuation
+    generated_password = ''.join(choice(characters) for _ in range(length))
+
+    return generated_password
