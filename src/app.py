db5c90c2805b8cef6f8e27aa338dd9d58a13b331
Vermeil <120065660+VermeilChan@users.noreply.github.com>
2024-05-27 01:05:53 +0100

    Fixed Code Formatting With Ruff :)

--- src/app.py
+++ src/app.py
@@ -3,20 +3,29 @@ from password_genrator import generate_password
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/generate', methods=['POST'])
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/generate", methods=["POST"])
 def generate():
-    password_length = int(request.form['length'])
-    include_uppercase = 'uppercase' in request.form
-    include_lowercase = 'lowercase' in request.form
-    include_digits = 'digits' in request.form
-    include_special = 'special' in request.form
-    password = generate_password(password_length, include_uppercase, include_lowercase, include_digits, include_special)
+    password_length = int(request.form["length"])
+    include_uppercase = "uppercase" in request.form
+    include_lowercase = "lowercase" in request.form
+    include_digits = "digits" in request.form
+    include_special = "special" in request.form
+    password = generate_password(
+        password_length,
+        include_uppercase,
+        include_lowercase,
+        include_digits,
+        include_special,
+    )
 
     return password
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run()
--- src/password_genrator.py
+++ src/password_genrator.py
@@ -1,8 +1,15 @@
 from secrets import choice
 from string import ascii_lowercase, ascii_uppercase, digits, punctuation
 
-def generate_password(length=16, include_uppercase=True, include_lowercase=True, include_digits=True, include_special=True):
-    characters = ''
+
+def generate_password(
+    length=16,
+    include_uppercase=True,
+    include_lowercase=True,
+    include_digits=True,
+    include_special=True,
+):
+    characters = ""
     if include_uppercase:
         characters += ascii_uppercase
     if include_lowercase:
@@ -11,6 +18,6 @@ def generate_password(length=16, include_uppercase=True, include_lowercase=True,
         characters += digits
     if include_special:
         characters += punctuation
-    generated_password = ''.join(choice(characters) for _ in range(length))
+    generated_password = "".join(choice(characters) for _ in range(length))
 
     return generated_password
